5793feef8edb30aed7d4de1348e8fbdf6796b44c
Moyinoluwa <moyinoluwaa@gmail.com>
2015-10-27 22:44:39 +0100

    Initial commit

---
+++ app/src/main/java/com/moyinoluwa/hellolagosdemo/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.moyinoluwa.hellolagosdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private AutoCompleteTextView mUsernameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.username);
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
